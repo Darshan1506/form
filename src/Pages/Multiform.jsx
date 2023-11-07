330492e2cd0a0c7259e15540cce78f1d3073a4b2
Pritipriya Singh <mail2pritipriya@gmail.com>
2023-11-08 01:05:49 +0530

    erros & validations fixed

--- src/Pages/Multiform.jsx
+++ src/Pages/Multiform.jsx
@@ -3,190 +3,39 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import { RxCross1 } from "react-icons/rx";
 import Upload from "../Components/Upload";
 import Range from "../Components/Range";
-const questions = [
-  {
-    id: 0,
-    ques: "Drop full name here",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 1,
-    ques: "Your LinkedIn URL",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 2,
-    ques: "Your Work Email Address",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 3,
-    ques: "What indistries are you interested in working?",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    options: [
-      "Sector Agnostic",
-      "B2B",
-      "B2C",
-      "AI/ML",
-      "ΑΡΙ",
-      "AR/VR",
-      "Creator Economy",
-      "Gaming",
-      "Analytics",
-      "Robotics",
-      "Saas",
-      "SpaceTech",
-      "SportsTech",
-      "Telecommunications",
-      "Travel and Tourism",
-      "Wearables",
-      "Insurtech",
-      "Agtech",
-      "Automation",
-      "BioTech",
-      "Cloud",
-      "Consumer Tech",
-      "Crypto/Blockchain",
-      "D2C",
-      "DeepTech",
-      "Developer Tools",
-      "Media and Entertainment",
-      "Mobility and Transportation",
-      "PropTech",
-      "Healthtech",
-      "Legal tech",
-      "Logistics and Supply Chain",
-      "Manufacturing",
-      "E-Commerce",
-      "Education",
-      "Climate Tech",
-      "Fintech",
-      "IoT",
-      "Clean Energy",
-      "Foodtech",
-      "HRtech",
-      "B2B Marketplace",
-      "others",
-      "E-sports",
-      "MarTech",
-      "MedTech",
-      "Retail Tech",
-      "others",
-    ],
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 4,
-    ques: "Your Area Of Expertise",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-    options: [
-      "Marketing",
-      "Finance & Fundraising",
-      "GTM",
-      "Human resources",
-      "Business strategy",
-      "Legal & Compliance",
-      "Human resources",
-      "International expansion",
-      "Network & Partnerships",
-      "Product development",
-      "Sales Network",
-      "Engineering",
-      "Customer success",
-      "Research & Market analysis",
-    ],
-  },
-  {
-    id: 5,
-    ques: "Your preferred cheque size",
-    range: true,
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 6,
-    ques: "Where do you work at?",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 7,
-    ques: "Do you invest via angel syndicates?",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    selectYesOrNo: true,
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 8,
-    ques: "Your porfolio companie",
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-  },
-  {
-    id: 9,
-    ques: "Upload your pic",
-    upload: true,
-    description:
-      "Add your email below to get notified as soon as it is available.",
-    placeholder: "yup yup",
-    err: "Hmm..Name is missing",
-  },
-];
+import { questions } from "../config/questions";
 
 function Multiform() {
-  const [error, setError] = useState([]);
+  const [error, setError] = useState([true]);
+  const [allErrors, setAllErrors] = useState(Array(10).fill(false));
   const [currentStep, setCurrentStep] = useState(() => {
     // Try to retrieve the currentStep value from localStorage
     const storedCurrentStep = localStorage.getItem("currentStep");
     return storedCurrentStep ? parseInt(storedCurrentStep, 10) : 0;
   });
-
-  const [updateError, setUpdateError] = useState([true]);
-  const [questionsPerStep, setQuestionsPerStep] = useState([3, 1, 3, 3]);
+  const [questionsPerStep, setQuestionsPerStep] = useState([3, 2, 3, 3]);
   const [formData, setFormData] = useState(() => {
     const storedData = localStorage.getItem("formData");
     return storedData ? JSON.parse(storedData) : [];
   });
 
-  const handleOptionClick = (e, index, option) => {
+  const handleOptionClick = (e, data, option) => {
     e.preventDefault();
     let updatedSelectedOptions;
-    console.log(Array.isArray(formData[index]));
-    if (Array.isArray(formData[index])) {
-      if (formData[index].includes(option)) {
-        updatedSelectedOptions = formData[index].filter(
+    console.log(Array.isArray(formData[data.id]));
+    if (Array.isArray(formData[data.id])) {
+      if (formData[data.id].includes(option)) {
+        updatedSelectedOptions = formData[data.id].filter(
           (item) => item !== option
         );
       } else {
-        updatedSelectedOptions = [...formData[index], option];
+        updatedSelectedOptions = [...formData[data.id], option];
       }
     } else {
       updatedSelectedOptions = [option];
     }
 
-    handleInputChange(index, updatedSelectedOptions);
+    handleInputChange(data, updatedSelectedOptions);
   };
 
   useEffect(() => {
@@ -217,32 +66,36 @@ function Multiform() {
     if (Array.isArray(error)) {
       console.log(updateError);
 
+
       ques.forEach((ques, index) => {
-        
-        if (
-          formData[ques.id] === undefined ||
-          formData[ques.id]?.length === 0
-        ) {
+
+        if (!ques.isOptional){
+          if(formData[ques.id] === undefined ||formData[ques.id]?.length === 0 ){
           console.log(formData[ques.id]);
           updateError[index] = true;
-        } else {
+
+          if(ques.id === 6){
+            updateError[index] = false;
+          }
+        }} else {
           console.log(formData[ques.id]);
+
           updateError[index] = false;
         }
         console.log("upd", updateError);
         setError(updateError);
         console.log("neww", error);
       }
-      
+
       );
     }
-  }, [currentStep, formData,  questionsPerStep.length]);
+  }, [currentStep, formData, questionsPerStep.length]);
 
   const handleNext = (e) => {
     e.preventDefault();
 
 
-    
+
     if (Array.isArray(error)) {
       if (!error.includes(true) && currentStep < totalSteps - 1) {
         setCurrentStep(currentStep + 1);
@@ -250,7 +103,7 @@ function Multiform() {
     }
   };
 
-  
+
   useEffect(() => {
     // Check the screen width and update isMobile state
     const checkScreenSize = () => {
@@ -258,7 +111,7 @@ function Multiform() {
       if (isMobile) {
         setQuestionsPerStep([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
       } else {
-        setQuestionsPerStep([3, 1, 3, 3]);
+        setQuestionsPerStep([3, 2, 3, 3]);
       }
     };
 
@@ -272,139 +125,160 @@ function Multiform() {
     };
   }, []);
 
-  const handleInputChange = (index, value) => {
+  const handleInputChange = (data, value) => {
     const updatedFormData = [...formData];
+    const updatedErrorValues = [...allErrors]
 
-    updatedFormData[index] = value;
-    setFormData(updatedFormData);
-  };
-  console.log(formData);
+    if (data.regex) {
+      if ((data.regex.test(value))) {
+        console.log(value, data.id, "no error")
+        updatedErrorValues[data.id] = false;
 
-  return (
-    <div className="w-full h-[100%] flex justify-center items-center">
-      <div className="relative border rounded-[10px] w-[70%] my-[10vh] border-[#E5E8F2] border-solid bottom-2">
-        <div className="mx-6 mt-6 flex justify-between">
-          <div className="flex gap-3">
-            <button
-              onClick={(e) => handlePrev(e)}
-              className="bg-[#F8F9FD] text-[#9DB4E6] px-2 text-[1rem]"
-            >{`<`}</button>
-            <button
-              onClick={(e) => handleNext(e)}
-              className="bg-[#F8F9FD] text-[#9DB4E6] px-2 text-[1rem]"
-            >{`>`}</button>
-          </div>
-          <button className="bg-[#F8F9FD] text-[#9DB4E6] px-2 text-[1rem]">
-            <RxCross1 />
-          </button>
-        </div>
-        {getCurrentQuestions().map((question, idx) => (
-          <div key={question.id} className="ml-[15%] mr-[13%] mt-[6%]">
-            <div className="flex flex-col gap-1 mb-12">
-              <label
-                htmlFor="campaignName"
-                className="font-inter text-[1.4rem] font-[400] leading-[1.6rem] text-[#507AD3] flex"
-              >
-                <div className="flex items-center gap-2 ml-[-2.4rem] justify-center mr-2">
-                  <h1 className="text-[1.1rem] font-inter font-[200]">
-                    {question.id + 1}
-                  </h1>
-                  <AiOutlineArrowRight size={15} />
-                </div>
-                {question.ques}
-              </label>
-              <p className="font-inter mb-2 text-[1.125rem] font-[200] text-[#5A81D9] opacity-[0.7]">
-                {question.description}
-              </p>
-              {question.options ? (
-                <div className="mb-12 flex flex-col gap-1">
-                  <div className="flex flex-wrap">
-                    {question.options.map((option, index) => (
-                      <button
-                        key={index}
-                        type="button" // Use type="button" to prevent form submission
-                        className={`m-[2px] p-1 text-[1rem]  ${
-                          formData[question.id]?.includes(option)
-                            ? "bg-[#c2d4f9] text-[#4A7BE5] border-[#4A7BE5] border-solid border-2 rounded-md"
-                            : "text-[#5A81D5D9] bg-[#E1E7F53D] border-[#E1E7F53D] border-2 rounded-md"
-                        }`}
-                        onClick={(e) =>
-                          handleOptionClick(e, question.id, option)
-                        }
-                      >
-                        {option}
-                      </button>
-                    ))}
-                  </div>
-                </div>
-              ) : question.selectYesOrNo ? (
-                <div className="flex flex-col gap-2">
-                  <button
-                    type="radio"
-                    onClick={() => handleInputChange(question.id, "Yes")}
-                    className={`${
-                      formData[question.id] === "Yes"
-                        ? "bg-gradient-to-r from-blue-300 via-blue-300 to-blue-300 bg-opacity-12 border-[#4A7BE5] border-2 rounded-md"
-                        : "bg-[#E1E7F53D] border-2 border-[#E1E7F53D]"
-                    } text-start p-2`}
-                  >
-                    Yes
-                  </button>
-                  <button
-                    type="radio"
-                    onClick={() => handleInputChange(question.id, "No")}
-                    className={`${
-                      formData[question.id] === "No"
-                        ? "bg-gradient-to-r from-blue-300 via-blue-300 to-blue-300 bg-opacity-12 border-[#4A7BE5] border-2 rounded-md"
-                        : "border-2 border-[#E1E7F53D]"
-                    } text-start p-2`}
-                  >
-                    No
-                  </button>
-                </div>
-              ) : question.upload ? (
-                <Upload />
-              ) : question.range ? (
-                <Range />
-              ) : (
-                <input
-                  id="campaignName"
-                  placeholder={`${question.placeholder}`}
-                  value={formData[question.id] || ""}
-                  onChange={(e) =>
-                    handleInputChange(question.id, e.target.value)
-                  }
-                  className="placeholder-[#b1c2e8] h-11 text-[#507AD3] font-inter text-[1.5rem]  border-b border-gray-300  focus:border-blue-500  focus:outline-none"
-                  required={true}
-                />
-              )}
-            </div>
-            {/* {console.log("jhedues", error[idx])} */}
-          </div>
-        ))}
+      } else {
+        console.log(value, data.id, "regex error there")
+        updatedErrorValues[data.id] = true;
+      }
+    }
+      else {
+        updatedErrorValues[data.id] = false;
+      }
+      updatedFormData[data.id] = value;
+      setFormData(updatedFormData);
+      setAllErrors(updatedErrorValues)
+    }
 
-        <div className="flex justify-between mx-6 mb-6 text-[#fff] p-2 text-[1rem]">
-          <div className="text-red-500">
-            {error?.includes(true) ? "All fileds are required" : ""}
-          </div>
-          <div>
-            {currentStep === totalSteps - 1 ? (
-              <button className="bg-[#4A7BE5] px-[18px] py-[12px] text-[18px] rounded-[5.926px] border border-solid ">
-                Submit Response
-              </button>
-            ) : (
+ 
+    console.log(formData);
+
+    return (
+      <div className="w-full h-[100%] flex justify-center items-center">
+        <div className="relative border rounded-[10px] w-[70%] my-[10vh] border-[#E5E8F2] border-solid bottom-2">
+          <div className="mx-6 mt-6 flex justify-between">
+            <div className="flex gap-3">
+              <button
+                onClick={(e) => handlePrev(e)}
+                className="bg-[#F8F9FD] text-[#9DB4E6] px-2 text-[1rem]"
+              >{`<`}</button>
               <button
                 onClick={(e) => handleNext(e)}
-                className="bg-[#4A7BE5] px-[18px] py-[12px] text-[18px] rounded-[5.926px] border border-solid "
-              >
-                Next
-              </button>
-            )}
+                className="bg-[#F8F9FD] text-[#9DB4E6] px-2 text-[1rem]"
+              >{`>`}</button>
+            </div>
+            <button className="bg-[#F8F9FD] text-[#9DB4E6] px-2 text-[1rem]">
+              <RxCross1 />
+            </button>
+          </div>
+          {getCurrentQuestions().map((question, idx) => (
+            <div key={question.id} className="ml-[15%] mr-[13%] mt-[6%]">
+              <div className="flex flex-col gap-1 mb-12">
+                <label
+                  htmlFor="campaignName"
+                  className="font-inter text-[1.4rem] font-[400] leading-[1.6rem] text-[#507AD3] flex"
+                >
+                  <div className="flex items-center gap-2 ml-[-2.4rem] justify-center mr-2">
+                    <h1 className="text-[1.1rem] font-inter font-[200]">
+                      {question.id + 1}
+                    </h1>
+                    <AiOutlineArrowRight size={15} />
+                  </div>
+                  {question.ques}
+                </label>
+                <p className="font-inter mb-2 text-[1.125rem] font-[200] text-[#5A81D9] opacity-[0.7]">
+                  {question.description}
+                </p>
+                {question.options ? (
+                  <div className="mb-12 flex flex-col gap-1">
+                    <div className="flex flex-wrap">
+                      {question.options.map((option, index) => (
+                       
+                        <button
+                          key={index}
+                          value={option}
+                          type="button" // Use type="button" to prevent form submission
+                          className={`m-[2px] p-1 text-[1rem]  ${formData[question.id]?.includes(option)
+                              ? "bg-[#c2d4f9] text-[#4A7BE5] border-[#4A7BE5] border-solid border-2 rounded-md"
+                              : "text-[#5A81D5D9] bg-[#E1E7F53D] border-[#E1E7F53D] border-2 rounded-md"
+                            }`}
+                          onClick={(e) =>
+                            handleOptionClick(e, question, option)
+                          }
+                        >
+                      
+                         
+                          {option}
+                        </button>
+                      ))}
+                    </div>
+                  </div>
+                ) : question.selectYesOrNo ? (
+                  <div className="flex flex-col gap-2">
+                    <button
+                      type="radio"
+                      onClick={() => handleInputChange(question, "Yes")}
+                      className={`${formData[question.id] === "Yes"
+                          ? "bg-gradient-to-r from-blue-300 via-blue-300 to-blue-300 bg-opacity-12 border-[#4A7BE5] border-2 rounded-md"
+                          : "bg-[#E1E7F53D] border-2 border-[#E1E7F53D]"
+                        } text-start p-2`}
+                    >
+                      Yes
+                    </button>
+                    <button
+                      type="radio"
+                      onClick={() => handleInputChange(question, "No")}
+                      className={`${formData[question.id] === "No"
+                          ? "bg-gradient-to-r from-blue-300 via-blue-300 to-blue-300 bg-opacity-12 border-[#4A7BE5] border-2 rounded-md"
+                          : "border-2 border-[#E1E7F53D]"
+                        } text-start p-2`}
+                    >
+                      No
+                    </button>
+                  </div>
+                ) : question.upload ? (
+                  <Upload />
+                ) : question.range ? (
+                  <Range />
+                ) : (
+                  <input
+                    id="campaignName"
+                    placeholder={`${question.placeholder}`}
+                    value={formData[question.id] || ""}
+                    onChange={(e) =>
+                      handleInputChange(question, e.target.value)
+                    }
+                    className="placeholder-[#b1c2e8] h-11 text-[#507AD3] font-inter text-[1.5rem]  border-b border-gray-300  focus:border-blue-500  focus:outline-none"
+                    required={true}
+                  />
+
+
+                )}
+                {allErrors[question.id] === true ? question.err : null}
+              </div>
+
+            </div>
+          ))}
+
+          <div className="flex justify-between mx-6 mb-6 text-[#fff] p-2 text-[1rem]">
+            <div className="text-red-500">
+              {/* {error?.includes(true) ? "All fileds are required" : ""} */}
+            </div>
+            <div>
+              {currentStep === totalSteps - 1 ? (
+                <button className="bg-[#4A7BE5] px-[18px] py-[12px] text-[18px] rounded-[5.926px] border border-solid ">
+                  Submit Response
+                </button>
+              ) : (
+                <button
+                  onClick={(e) => handleNext(e)}
+                  className="bg-[#4A7BE5] px-[18px] py-[12px] text-[18px] rounded-[5.926px] border border-solid "
+                >
+                  Next
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Multiform;
+  export default Multiform;
