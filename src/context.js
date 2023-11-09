17f5e8161664646f22f7f27894a702b0e98a49ba
Darshan Rajpurohit <darshanrajpurohit47@gmail.com>
2023-11-09 20:11:41 +0530

    feature

--- src/context.js
+++ src/context.js
@@ -33,7 +33,7 @@ const totalSteps = questionsPerStep.length;
 
 const handleInputChange = (data, value) => {
   const updatedFormData = [...formData];
-  // const updatedErrorValues = [...allErrors];
+  const updatedErrorValues = [...allErrors];
 
   // Clear the previous validation timer
 
@@ -41,34 +41,34 @@ const handleInputChange = (data, value) => {
   // Set a new timer for validation after a delay (e.g., 1000 milliseconds)
 
 
-    // if (data.regex) {
-    //   if(value){
-    //     if (data.regex.test(value)) {
+    if (data.regex) {
+      if(value){
+        if (data.regex.test(value)) {
 
 
-    //     console.log(value, data.id, "no error");
-    //     updatedErrorValues[data.id] = false;
-    //   } else {
-    //     console.log(value, data.id, "regex error there");
-    //     updatedErrorValues[data.id] = true;
-    //   }
-    //   }else{
-    //     updatedErrorValues[data.id] = false;
-    //   }
+        console.log(value, data.id, "no error");
+        updatedErrorValues[data.id] = false;
+      } else {
+        console.log(value, data.id, "regex error there");
+        // updatedErrorValues[data.id] = true;
+      }
+      }else{
+        updatedErrorValues[data.id] = false;
+      }
       
-    // } else {
-    //   // Handle other input types or empty values
-    //   if (value.length === 0) {
-    //     updatedErrorValues[data.id] =false;
-    //   } else {
-    //     updatedErrorValues[data.id] = false;
-    //   }
-    // }
+    } else {
+      // Handle other input types or empty values
+      if (value.length === 0) {
+        updatedErrorValues[data.id] =false;
+      } else {
+        updatedErrorValues[data.id] = false;
+      }
+    }
 
     // Update formData and error values after validation
     updatedFormData[data.id] = value;
     setFormData(updatedFormData);
-    // setAllErrors(updatedErrorValues);
+    setAllErrors(updatedErrorValues);
  // Adjust the delay as needed
 };
 
